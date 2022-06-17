2bb4bf4e5947ed596ff09d6de5f20425f45c1bf5
elvis <cenelvis@gmail.com>
2022-06-16 23:34:44 -0400

    Using Thunk function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useSelector, useDispatch } from "react-redux";
 import React, { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true; // Places outside "App()" So it wont reset when is called again
 
@@ -14,53 +14,14 @@ function App() {
   const cart = useSelector((state) => state.cart); // Get cart data
   const notification = useSelector((state) => state.ui.notification); // Extracting value from slice (ui because of "index.js")
 
-  // useEffect() => automatically run at the end when [cart] has changes
-  // useEffect() => cannot use async (but a function inside can)
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending ...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-58740-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart), // send/convert data to JSON
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart Data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-
-    // Preventing useEffect() to run for the first load
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart Data failed",
-        })
-      );
-    });
+    // sendCartData() => Thunk Function (To call the function in another component, so this component in cleaner)
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const initialCartState = {
   items: [],
@@ -49,6 +50,52 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// Thunk Function (To avoid adding too much code/functionality in "App.js")
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending ...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-58740-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart), // send/convert data to JSON
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart Data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart Data failed",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions; // For dispatching
 
